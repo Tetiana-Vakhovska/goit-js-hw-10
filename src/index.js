e7ea5aa4ff03427771f1a7ff79a9f50f0f1830d8
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-13 21:34:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ new SlimSelect({
 axios.defaults.headers.common["x-api-key"] = "live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn";
 const elements={
   form:document.querySelector('.js-breedId'),
-  container:document.querySelector('.js-breedId-list')
+  container:document.querySelector('.js-breedId-list'),
+  div:document.querySelector('.js-div.cat-info'),
 
 }
 function fetchBreeds(){
