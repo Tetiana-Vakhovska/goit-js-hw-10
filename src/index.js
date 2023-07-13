e5bca90d2c63b4e0f06ee4ec952266a6da0081a7
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-13 10:07:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ new SlimSelect({
 })
 
 axios.defaults.headers.common["x-api-key"] = "live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn";
+const elements={
+  form:document.querySelector('.js-breedId'),
+  container:document.querySelector('.js-breedId-list')
 
+}
 function fetchBreeds(){
     const BASE_URL= 'https://api.thecatapi.com/v1/breeds';
 
