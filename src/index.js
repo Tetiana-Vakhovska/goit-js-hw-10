c9619c302a697d3ff159cb28bd361236f8b0e57a
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-13 09:49:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-ggggg
+import axios from "axios";
+import notiflix from "notiflix";
+import SlimSelect from 'slim-select'
+
+new SlimSelect({
+  select: '#selectElement'
+})
+
+axios.defaults.headers.common["x-api-key"] = "live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn";
+
+function fetchBreeds(){
+    const BASE_URL= 'https://api.thecatapi.com/v1/breeds';
+
+};
+function fetchCatByBreed(breedId){};
