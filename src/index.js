9fc2329d8996df3c128cda3d2fa594647517e1dd
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-13 22:46:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import axios from "axios";
 import notiflix from "notiflix";
 import SlimSelect from 'slim-select'
+import loader from "css-loader";
 
 new SlimSelect({
   select: '#selectElement'
 })
 
 axios.defaults.headers.common["x-api-key"] = "live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn";
-const elements={
-  form:document.querySelector('.js-breedId'),
-  container:document.querySelector('.js-breedId-list'),
-  div:document.querySelector('.js-div.cat-info'),
 
-}
+ const select = document.querySelector('.js-breed-select');
+  const container=document.querySelector('.js-breedId-list');
+  const infoCat =document.querySelector('.js-cat-info');
+  
+
+
 function fetchBreeds(){
     const BASE_URL= 'https://api.thecatapi.com/v1/breeds';
+   const ENDPOINT=id;
 
 };
-function fetchCatByBreed(breedId){};
+function fetchCatByBreed(evt){
+evt.preventDefault();
+const infoCat= evt.target.value.trim();
+};
