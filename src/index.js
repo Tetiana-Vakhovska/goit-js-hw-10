bd2ebde7c2a7b29813685b4baec73fa7c52489bc
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-18 10:45:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import axios from "axios";
 import notiflix from "notiflix";
 
 import loader from "css-loader";
+import {fetchBreeds} from './cat-api';
+import {fetchCatByBreed} from './cat-api';
+  
 
 import SlimSelect from 'slim-select';
 
- let allCat
+
 new SlimSelect({
   select: '#selectElement',
 })
 
- const breed_ids=id;
- const  baseURL = 'https://some-domain.com/api/';
 
 axios.defaults.headers.common["x-api-key"] = "live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn";
 axios.defaults.baseURL='https://api.thecatapi.com/v1/breeds';
@@ -32,19 +33,7 @@ export function fetchBreeds(){
       return (Response.status!==response.data);
     })
   }
-  axios.get('/user?ID=12345')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
- 
+  
 axios.get('/user/12345')
   .then(function (response) {
     console.log(response.data);
