2bd4f0efd7223ee40c7820d2992da739d6597de1
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-18 10:45:36 +0300

    Create cat-api.js

---
+++ src/cat-api.js
@@ -0,0 +1,16 @@
+import { join } from "path-browserify";
+
+function fetchBreeds(){
+
+    const BASE_URL =' https://api.thecatapi.com/v1/breeds' ;
+    const END_POINT = '/trending/movie/week ';
+    const API_KEY = "live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn";
+    return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}&page=${page}`)
+    .then( resp=>{
+        if (!resp.ok){
+            throw new Error(resp.statusText);
+        }
+        return resp.json();
+        console.log(Response)
+    })
+}
