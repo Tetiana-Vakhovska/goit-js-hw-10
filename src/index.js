3a5f4140c22bdbbfe9dd70d03c49ad8f2246a17c
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-18 12:19:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,22 +9,50 @@ import {fetchCatByBreed} from './cat-api';
 import SlimSelect from 'slim-select';
 
 
-new SlimSelect({
-  select: '#selectElement',
-})
+
 const selectIn = document.querySelector('.breed-select');
 const catItem = document.querySelector('.cat-info');
 const loardEL= document.querySelector('.loard');
 const errorEl = document.querySelector('.error');
 
+loardEL.classList.replace('loadEl','is-hidden');
+errorEl.classList.add('is-hidden');
+divCatInfo.classList.add('is-hidden');
+ let arrBreedsId=[];
+ fetchBreeds()
+ then.axios(data=> {
+  arrBreedsId.push({Text:Element.name,value:Element.id});
+  new SlimSelect({
+    select: selectIn,
+    data:arrBreedsId
+  });
+ })
+.catch(onFechError);
 
- let allCatArray={};
+selectIn.addEventListener('change', onSelectBreed);
+function onSelectBreed (event){
+  loardEL.classList.replace('is-hidden','loadEl');
+selectIn.classList.add('is-hidden');
+divCatInfo.classList.add('is-hidden');
+}
 
-
-
+const breedId = event.currentTarget.value;
+fetchCatByBreed(breedId)
+then.axios(data=>{
+  loardEL.classList.replace('loadEl', 'is-hidden');
+  selectIn.classList.remove('is-hidden');
+  const {url, breeds}=data[0];
+})
 axios.defaults.headers.common["x-api-key"] = "live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn";
 axios.defaults.baseURL='https://api.thecatapi.com/v1/breeds';
-
+ axios.get(`https://api.thecatapi.com/v1/breeds_ids=${breedId}`)
+ .then(({data})=>
+ {return data;}
+ )
+.catch ((error)=>{
+  console.log('error', error);
+}
+)
 function fetchBreeds(){
  
   }
