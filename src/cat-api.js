43044e5a45f83e87d645890f978ec42abc7b4171
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-20 10:31:41 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,25 +1,25 @@
 
-const options ={Headers:{
-    "x-api-key":'live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn'
+const options ={headers:{
+    "key":'live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn'
 }
 }
  export function fetchBreeds(){
-    return fetch('https://api.thecatapi.com/v1/breeds', options)
-    .then( Response=>{
-        if (!Response.ok){
-            throw new Error(Response.statusText);
+    return fetch('${key}', options)
+    .then( response=>{
+        if (!response.ok){
+            throw new Error(response.statusText);
         }
-        return Response.json();
+        return response.json();
         
     })
 }
 export function fetchCatByBreed(breedId){
-    return fetch('https://api.thecatapi.com/v1/images/search_ids={breed.id}' , options)
-    .then( (Response)=>{
-        if (!Response.ok){
-            throw new Error(Response.statusText);
+    return fetch('https://api.thecatapi.com/v1/images/search_ids=${breed.id}' , options)
+    .then( (response)=>{
+        if (!response.ok){
+            throw new Error(response.statusText);
         }
-        return Response.json();
+        return response.json();
 
 
 })}
