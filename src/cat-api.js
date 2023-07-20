771d07741cae1cff15534df5d5be728bd7ea9165
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-07-20 11:03:52 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,23 +1,21 @@
-
-const options ={headers:{
-    "key":'live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn'
-}
-}
+import axios from "axios";
+axios.defaults.headers.common['x-api-key']='live_7u1513J1pLH6k4dsxR0sDujpDXqykt0NqVRRGuZsbLcOTa3MmbJ082NYemF7E1jn';
+axios.defaults.baseURL='https://api.thecatapi.com/v1/'
  export function fetchBreeds(){
-    return fetch(`${key}`, options)
+    return axios.get('./breeds/')
     .then( response=>{
         if (!response.ok){
-            throw new Error(response.statusText);
+            throw new error(response.statusText);
         }
         return response.json();
         
     })
 }
 export function fetchCatByBreed(breedId){
-    return fetch(`https://api.thecatapi.com/v1/images/search_ids=${breed.id}` , options)
+    return axios.get(`/images/search?breed_ids=${breedId}`)
     .then( (response)=>{
         if (!response.ok){
-            throw new Error(response.statusText);
+            throw new error(response.statusText);
         }
         return response.json();
 
